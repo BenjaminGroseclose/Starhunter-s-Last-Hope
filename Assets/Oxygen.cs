ce52c33a56652b3f4c33cb301201fa88a71e7c04
Grose1bm <benjamin.groseclose@hotmail.com>
2017-11-22 15:14:32 -0500

    Initial Commit
    Initial Commit

---
+++ Assets/Oxygen.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Threading;
+
+public class Oxygen : MonoBehaviour {
+
+	public Transform other;
+	public int oxygenLevel;
+	public int initialOxygenLevel;
+	public Text oxygenText;
+
+	// Determines the distance between the player a object that will determine
+	// whether they are able to be in the oxygen zone or not. 
+	float DistanceCalc(){
+			float dist = Vector3.Distance (other.position, transform.position);
+			return dist;
+	}
+		
+
+	// Use this for initialization
+	void Start () {
+		initialOxygenLevel = oxygenLevel;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		decimal percentage;
+
+		if (DistanceCalc () >= 40.0) {
+			oxygenLevel = oxygenLevel - 1;
+		}
+
+		percentage = (decimal.Divide(oxygenLevel, initialOxygenLevel)); 
+		oxygenText.text = "Oxygen Level: " + percentage.ToString("P");
+
+
+		if (oxygenLevel < 0) {
+			// Game Over. 
+		}
+
+
+	}
+}
---
+++ Assets/skyboxRotationScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class skyboxRotationScript : MonoBehaviour {
+	public float rot = 0;
+	public Skybox sky;
+
+	// Use this for initialization
+	void Start () {
+		sky = GetComponent<Skybox> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		rot += 2 * Time.deltaTime;
+		rot %= 360;
+		sky.material.SetFloat ("_Rotation", rot);
+	}
+}
+
