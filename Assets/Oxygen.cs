7cba580b982eca26e9b60362c50627d3cf52ebee
Benjamin Groseclose <benjamin.groseclose@hotmail.com>
2017-11-28 00:55:24 -0500

    Contiuned Changes
    Add more UI features, force Field object (needs more work)
    
    Also added the new spaceship object.

--- Assets/Oxygen.cs
+++ Assets/Oxygen.cs
@@ -6,34 +6,37 @@ using System.Threading;
 
 public class Oxygen : MonoBehaviour {
 
-	public Transform other;
-	public int oxygenLevel;
-	public int initialOxygenLevel;
+	public Transform spaceShip;
+	public float oxygenLevel;
 	public Text oxygenText;
+    public Slider oxygenSlider;
 
 	// Determines the distance between the player a object that will determine
 	// whether they are able to be in the oxygen zone or not. 
 	float DistanceCalc(){
-			float dist = Vector3.Distance (other.position, transform.position);
+			float dist = Vector3.Distance (spaceShip.position, transform.position);
 			return dist;
 	}
 		
 
 	// Use this for initialization
 	void Start () {
-		initialOxygenLevel = oxygenLevel;
+        oxygenSlider.value = 100;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		decimal percentage;
 
 		if (DistanceCalc () >= 40.0) {
-			oxygenLevel = oxygenLevel - 1;
+			oxygenLevel = oxygenLevel - (float) .005;
 		}
 
-		percentage = (decimal.Divide(oxygenLevel, initialOxygenLevel)); 
-		oxygenText.text = "Oxygen Level: " + percentage.ToString("P");
+        oxygenSlider.value = oxygenLevel;
+
+        oxygenText.text = oxygenLevel.ToString("n2") + "%";
+
+		//percentage = (decimal.Divide(oxygenLevel, initialOxygenLevel)); 
+		//oxygenText.text = "Oxygen Level: " + percentage.ToString("P");
 
 
 		if (oxygenLevel < 0) {
---
+++ Assets/UIScript.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScript : MonoBehaviour {
+
+    public Button startButton;
+    public Button quitButton;
+    public GameObject panel;
+
+	// Use this for initialization
+	void Start () {
+
+        /*
+         * Button btn = yourButton.GetComponent<Button>();
+         * btn.onClick.AddListener(TaskOnClick);
+         */
+        panel = GameObject.Find("startUIPanel");
+
+        Button startBtn = startButton.GetComponent<Button>();
+        Button quitBtn = quitButton.GetComponent<Button>();
+
+        startBtn.onClick.AddListener(startButtonOnClick);
+        quitBtn.onClick.AddListener(quitButtonOnClick);
+
+
+    }
+
+    private void quitButtonOnClick()
+    {
+        panel.SetActive(false);
+    }
+
+    private void startButtonOnClick()
+    {
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+
+}
